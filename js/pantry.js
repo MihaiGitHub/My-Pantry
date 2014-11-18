db75998839a9efbd3956f626b7d5d2e4ad1c33ba
Mihai <mihailiviu@yahoo.com>
2014-11-17 23:01:35 -0700

    Added edit client view and created controller

--- js/pantry.js
+++ js/pantry.js
@@ -7,11 +7,12 @@ pantryApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
   $routeProvider.when('/search', {templateUrl: 'partials/searchclient.html', controller: 'navCtrl'});
   $routeProvider.when('/add', {templateUrl: 'partials/addclient.html'});
+  $routeProvider.when('/edit', {templateUrl: 'partials/editclient.html'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
 
 pantryApp.run(function($rootScope, $location, loginService){
-	var routespermission=['/search', '/add'];  //route that require login
+	var routespermission=['/search', '/add', '/edit'];  //route that require login
 	
 	$rootScope.$on('$routeChangeStart', function(){
 		if( routespermission.indexOf($location.path()) !=-1)
@@ -49,55 +50,37 @@ pantryApp.controller('loginCtrl', ['$scope','$location','loginService', function
     };
 	
 }]);
-/***********************SERVICES***************/
-pantryApp.factory('loginService',function($rootScope, $http, $location, sessionService){
-	return{
-		login:function(data,scope){
-			var $promise=$http.post('php/user.php',data); //send data to user.php
-			$promise.then(function(msg){
-				var uid=msg.data;
-				if(uid){
-					$rootScope.authenticated = true;
-					sessionService.set('uid',uid);
-					
-					$location.path('/search');
-				}	       
-				else  {
-					scope.msgtxt='incorrect information';
-					$location.path('/login');
-				}				   
-			});
-		},
-		logout:function(){
-			sessionService.destroy('uid');
-			$location.path('/login');
-		},
-		islogged:function(){
-			var $checkSessionServer=$http.post('php/check_session.php');
-			return $checkSessionServer;
-			/*
-			if(sessionService.get('user')) return true;
-			else return false;
-			*/
-		}
-	}
+pantryApp.controller('editController', function ($scope, $http){ 
+	var urlSearch = 'php/edit.php';	
+	$scope.clientID = '1234';
+	
+	/*
+	$scope.edit = function(){ 
+		var thisData = "id=" + $scope.clientID;
+		thisData += "&fname=" + $scope.fname;
+		thisData += "&lname=" + $scope.lname;
+				
+		$http({
+			method: 'POST',
+			url: urlSearch,
+			data: thisData,
+			headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
+		})
+		
+		.success(function(data, status) {
+			if(data.clients){ 
+				$scope.clients = data.clients;			
+			}
+		})
+		
+		.error(function(data, status) {
+			$scope.data = data || "Request failed";
+			$scope.status = status;			
+		});	
 
-});
-pantryApp.factory('sessionService', ['$http', function($http){
-	return{
-		set:function(key,value){
-			return sessionStorage.setItem(key,value);
-		},
-		get:function(key){
-			return sessionStorage.getItem(key);
-		},
-		destroy:function(key){
-			$http.post('php/destroy_session.php');
-			return sessionStorage.removeItem(key);
-		}
 	};
-}])
-
+	*/
+});
 
 pantryApp.controller('searchController', function ($scope, $http){ 
 	var urlSearch = 'php/search.php';	
@@ -186,4 +169,52 @@ pantryApp.controller('navCtrl', ['$scope', '$location', function ($scope, $locat
         return viewLocation === $location.path();
     };
 	
+}]);
+/***********************SERVICES***************/
+pantryApp.factory('loginService',function($rootScope, $http, $location, sessionService){
+	return{
+		login:function(data,scope){
+			var $promise=$http.post('php/user.php',data); //send data to user.php
+			$promise.then(function(msg){
+				var uid=msg.data;
+				if(uid){
+					$rootScope.authenticated = true;
+					sessionService.set('uid',uid);
+					
+					$location.path('/search');
+				}	       
+				else  {
+					scope.msgtxt='incorrect information';
+					$location.path('/login');
+				}				   
+			});
+		},
+		logout:function(){
+			sessionService.destroy('uid');
+			$location.path('/login');
+		},
+		islogged:function(){
+			var $checkSessionServer=$http.post('php/check_session.php');
+			return $checkSessionServer;
+			/*
+			if(sessionService.get('user')) return true;
+			else return false;
+			*/
+		}
+	}
+
+});
+pantryApp.factory('sessionService', ['$http', function($http){
+	return{
+		set:function(key,value){
+			return sessionStorage.setItem(key,value);
+		},
+		get:function(key){
+			return sessionStorage.getItem(key);
+		},
+		destroy:function(key){
+			$http.post('php/destroy_session.php');
+			return sessionStorage.removeItem(key);
+		}
+	};
 }]);
